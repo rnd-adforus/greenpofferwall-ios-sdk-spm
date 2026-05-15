197b1dfd7600a6b88194329fe19d93e948a9c152
SungEunAdforus <jennet@adforus.co.kr>
2026-05-15 13:37:41 +0900

    Release version 4.2.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.9
 import PackageDescription
 
-let versionInfo = "4.2.0"
+let versionInfo = "4.2.1"
 let package = Package(
     name: "GreenPOfferWall",
     platforms: [
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "GreenPOfferWall",
             url: "https://nexus.adforus.com/repository/greenp_iOS/GreenPOfferWall/\(versionInfo)/GreenpOfferwall.xcframework.zip",
-            checksum: "378cc66b0dd0d0df1b6212dd5f51c525425638edc969eadf5b39f734a6185c0e"
+            checksum: "2670c51668e9d59493592c5c3028661b76e0b8aec690ec48cdc48aa3520474a8"
         )
     ]
 )
